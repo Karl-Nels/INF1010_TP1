2e722852d9bd13584e9c94962942c0204d4d88eb
Karl Nelson SOMO OSSOAL <karl.nels@yahoo.fr>
2018-01-26 11:23:27 -0500

    MAJ Panier.cpp

--- Panier.cpp
+++ Panier.cpp
@@ -4,6 +4,52 @@
 using namespace std;
 
 Panier::Panier(int capacite):capaciteContenu_(capacite),nombreContenu_(0),totalAPayer_(0.0), contenuPanier_(new Produit *[capacite]){
-	fenjiromohamed1@gmail.com
+
+}
+
+Produit ** Panier::obtenirContenuPanier() {
+
+	return contenuPanier_;
+}
+
+int Panier::obtenirNombreContenu() {
+
+	return nombreContenu_;
+}
+
+double Panier::obtenirTotalApayer() {
+
+	return totalAPayer_;
+}
+
+void Panier::ajouter(Produit * prod) {
+
+	if (capaciteContenu_ == nombreContenu_) {
+
+		Produit ** copie = contenuPanier_;
+		capaciteContenu_ *= 2;
+		contenuPanier_ = new Produit*[capaciteContenu_];
+		contenuPanier_ = copie;
+		delete copie;
+	}
+	contenuPanier_[nombreContenu_++] = prod;
+	totalAPayer_ += contenuPanier_[nombreContenu_]->obtenirPrix();
+}
+
+void Panier::livrer() {
+
+	for (unsigned i = 0; i < nombreContenu_; i++) {
+		delete contenuPanier_[i];
+	}
+
+	delete contenuPanier_;
+	contenuPanier_ = nullptr;
+	nombreContenu_ = 0;
+	totalAPayer_ = 0.0;
+	contenuPanier_ = new Produit*[capaciteContenu_];
+
+}
+void Panier::afficher() {
+
 
 }
